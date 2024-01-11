8e64cf261fadfab35dbdfe86f3620807ad991f75
abulKhasim <abulkhasim999@gmail.com>
2024-01-11 21:36:22 +0530

    completed mongo part of week-3 assignment

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,24 @@
+const {User} = require("../db/index");
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    User.findOne({
+        username:username,
+        password:password
+    }).then(function(val) {
+        if(val) {
+            next();
+        } else {
+            res.status(403).json({
+                msg:"User doesnt exit!!"
+            })
+        }
+    })
 }
 
 module.exports = userMiddleware;
